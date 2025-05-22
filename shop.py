e110de4500e0d3df25799fae05a2adb9fb0a51c5
Jettimestari <vormalaa@gmail.com>
2025-05-22 12:02:36 +0300

    Update shop.py

--- shop.py
+++ shop.py
@@ -1,10 +1,10 @@
 # Shop
 
 shop=[
-    {"name":"","price":50,"inflate":1.01},
-    {"name":"","price":50,"inflate":1.01},
-    {"name":"","price":50,"inflate":1.01},
-    {"name":"","price":50,"inflate":1.01},
-    {"name":"","price":50,"inflate":1.01},
-    {"name":"","price":50,"inflate":1.01}
+    {"name":"Puutarhuri","price":50,"inflate":1.01},
+    {"name":"Vapaaehtoistyöntekijä","price":250,"inflate":1.01},
+    {"name":"Luonnonsuojeluyhdistys","price":10000,"inflate":1.01},
+    {"name":"Hallintolaki","price":1000000,"inflate":1.01},
+    {"name":"Maailmanlaajuinen liike","price":50000000,"inflate":1.01},
+    {"name":"Luonnonjumala","price":1000000000,"inflate":1.01}
 ]

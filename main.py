65b6654282bd43368ae9d83f835c7ac36daa170d
Spookie0 <satan24562@gmail.com>
2025-05-22 13:53:32 +0300

    Commit

--- main.py
+++ main.py
@@ -6,16 +6,13 @@ import shop
 pygame.init() #init pygame
 fps=60 #fps
 fpsClock=pygame.time.Clock() #init fps clock
-size=(700,500) #windows size
+size=(900,700) #windows size
 font_counter=pygame.font.SysFont('Arial Black',32)
-
 screen=pygame.display.set_mode(size)
 pygame.display.set_caption("My Game")
 
-button_rect=pygame.Rect(300,150,100,100)
+button_rect=pygame.Rect(400,200,100,100)
 counter=0 #The Global Counter
-
-quitbutton_rect=pygame.Rect(0,0,40,20)
 
 with open("data/dat","r",encoding="utf-8") as file:
     counter=int(file.read())
@@ -24,27 +21,23 @@ counter_text=font_counter.render(str(counter),True,(255,255,255))
 
 #images
 earth=pygame.image.load("resources/earth.png").convert_alpha()
-quit=pygame.image.load("resources/quit.png").convert_alpha()
+
+print(shop.shop_list[0]["name"])
 
 done=False;
-
-
 while not done:
     for event in pygame.event.get():
         if event.type==pygame.QUIT:
             done=True
-        if event.type==MOUSEBUTTONDOWN:
+        elif event.type==MOUSEBUTTONDOWN:
             if button_rect.collidepoint(event.pos):
                 counter+=1
                 counter_text=font_counter.render(str(counter),True,(255,255,255))
                 print(counter)
-            elif quitbutton_rect.collidepoint(event.pos):
-                done=True #save&quit
 
     #screen
     screen.fill((0,0,0))
-    screen.blit(earth,(300,150))
-    screen.blit(quit,(0,0))
+    screen.blit(earth,(400,200))
     screen.blit(counter_text,(300,300))
     pygame.display.update()
 
--- shop.py
+++ shop.py
@@ -1,10 +1,10 @@
 # Shop
 
-shop=[
-    {"name":"Puutarhuri","price":50,"inflate":1.01},
-    {"name":"Vapaaehtoistyöntekijä","price":250,"inflate":1.01},
-    {"name":"Luonnonsuojeluyhdistys","price":10000,"inflate":1.01},
-    {"name":"Hallintolaki","price":1000000,"inflate":1.01},
-    {"name":"Maailmanlaajuinen liike","price":50000000,"inflate":1.01},
-    {"name":"Luonnonjumala","price":1000000000,"inflate":1.01}
+shop_list=[
+    {"name":"Volunteer","price":50,"inflate":1.01,"png":"resources/ShopVolunteer.png"},
+    {"name":"Gardener","price":250,"inflate":1.01,"png":"resources/ShopGardener.png"},
+    {"name":"Forester","price":10000,"inflate":1.01,"png":"resources/ShopForester.png"},
+    {"name":"Dryad","price":1000000,"inflate":1.01,"png":"resources/ShopDryad.png"},
+    {"name":"Specialist","price":50000000,"inflate":1.01,"png":"resources/ShopSpecialist.png"},
+    {"name":"God Of Nature","price":250000000,"inflate":1.01,"png":"resources/ShopGodOfNature.png"}
 ]
